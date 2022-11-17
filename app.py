cbd9237859f0ec0c6db401e589f847bf26cbe8ea
Celia Chang <tina8899530@gmail.com>
2022-11-17 21:26:38 +0800

    modify delete ifmain

--- app.py
+++ app.py
@@ -159,5 +159,5 @@ def booking():
 def thankyou():
 	return render_template("thankyou.html")
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000, debug=True)
+
+app.run(host='0.0.0.0', port=3000, debug=True)
