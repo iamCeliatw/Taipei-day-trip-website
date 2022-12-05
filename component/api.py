8875511c6966a58eb27304bcc08951b184c59009
Celia Chang <tina8899530@gmail.com>
2022-12-06 00:00:33 +0800

    week4 First Commit

--- component/api.py
+++ component/api.py
@@ -1,9 +1,12 @@
+from datetime import datetime, timedelta, timezone
+
+import jwt
 from flask import *
 
-from database import db
+from component.database import db
 
 #blueprint setup
-api = Blueprint('api',__name__, url_prefix='/api')
+api = Blueprint('myApi',__name__, url_prefix='/api')
  
 @api.route('/categories')
 def cat():
@@ -17,13 +20,11 @@ def cat():
         catList = []
         for i in result:
             catList.append(i[0])
-        print(catList)
         cursor.close()
         conn.close()
         return jsonify({'data':catList})
     except:
         return jsonify({'error': True,'message':"伺服器內部錯誤"}),500
-
 
 @api.route('/attractions')
 def attraction_spot():
@@ -31,7 +32,7 @@ def attraction_spot():
         args = request.args
         page = args.get("page", type=int, default=0)
         keyword = args.get("keyword", default='')
-        if keyword is '':
+        if keyword == '':
             perpage = 13
             # 第一頁 取 0~13 第二頁 取 第13~25（13筆）
             offset = page * (perpage-1)
@@ -94,7 +95,6 @@ def attraction(id):
         cursor = conn.cursor(buffered=True)
         cursor.execute(sql1, val1)
         result = cursor.fetchone()
-        print(result)
         if result is not None:
             str = ''.join(result)
             img = str.split('https://')
@@ -107,10 +107,8 @@ def attraction(id):
             cursor = conn.cursor(buffered=True, dictionary=True)
             cursor.execute(sql2, val2)
             all_result = cursor.fetchone()
-            print(type(all_result))
             # 新增照片網址的陣列
             all_result.setdefault('image', arrImg)
-            print(all_result)
             cursor.close()
             conn.close()
             return jsonify({'data': all_result})
@@ -120,3 +118,90 @@ def attraction(id):
             return jsonify({"error": True, "message": "伺服器內部錯誤"}), 500
 
 
+
+@api.route('user', methods=["POST"])
+def user():
+    try:
+        data = request.get_json()
+        name = data['name']
+        email = data['email']
+        password = data['password']
+        # print(data)
+        sql = 'SELECT * FROM member WHERE email = %s'
+        val = [email]
+        conn = db.connection.get_connection()
+        cursor = conn.cursor()
+        cursor.execute(sql,val)
+        user = cursor.fetchone()
+        # cursor.close()
+        if name == '' or email == '' or password == '':
+            return {"error": True, "message": "欄位不得為空"}, 400
+        elif user:
+            return {"error": True, "message": "信箱已被註冊"}, 400
+        sql = "INSERT INTO member (name, email, password) VALUES (%s, %s, %s)"
+        val = [name, email, password]
+        cursor.execute(sql, val)
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return {"ok":True,  "message": "您已註冊成功✅，請登入"}
+    except:
+        return {"error": True, "message": "伺服器內部錯誤"}, 500
+
+@api.route('user/auth',methods=["GET","PUT","DELETE"])
+def user_auth():
+    try:
+     # 轉換時區 -> 東八區  
+        dt1 = datetime.utcnow().replace(tzinfo=timezone.utc)
+        now = dt1.astimezone(timezone(timedelta(hours=8))) 
+        delta = timedelta(days=7)
+        n_days = now + delta
+        secret = "wdfm3lmlask3"
+        if request.method == 'PUT':
+            data = request.get_json()
+            userMail = data['email']
+            password = data['password']
+            if userMail == '' or password == '':
+                return {"error":True,"message":"欄位不得為空"},400
+            sql = "SELECT id, name, email, password FROM member WHERE email = %s AND password = %s "
+            val = [userMail,password]
+            conn = db.connection.get_connection()
+            cursor = conn.cursor(buffered=True, dictionary=True)
+            cursor.execute(sql,val)
+            cursor.close()
+            conn.close()
+            result = cursor.fetchone()
+            #要把使用者資訊帶入 payload
+            if result is None:
+                return {"error":True,"message":"Email不存在"},400
+            payload = {
+            "id":result['id'],
+            "iat":now,
+            "exp":n_days,
+            }
+            encoded_jwt = jwt.encode(payload, secret, algorithm="HS256")
+            resp = make_response({"ok":True})
+            resp.set_cookie(key='token',value=encoded_jwt)
+            # resp.headers['Set-Cookie'] = encoded_jwt
+            return resp
+        elif request.method == 'GET':
+            #確認token尚未過期 取得cookie
+            resp = request.cookies.get("token") 
+            print(resp) 
+            if resp is None:
+                return {"data":None}
+            decode_jwt = jwt.decode(resp, secret, algorithms=["HS256"])
+            if decode_jwt:
+                return {"data":decode_jwt}
+
+        elif request.method == 'DELETE':
+            #刪除cookie
+            resp = make_response({"ok":True})
+            resp.set_cookie(key='token', value='', expires=0)
+            return resp
+    except:
+        return {"error": True, "message": "伺服器內部錯誤"}, 500
+
+
+       
+        #如果帳密是對的話 JWT SET cookie 
--- static/styles/js/attraction.js
+++ static/styles/js/attraction.js
@@ -1,52 +1,169 @@
 "use strict";
 
 //取網址最後一個字當作id值
-let id = location.pathname.substring(location.pathname.lastIndexOf("/") + 1);
-let carousel__nav = document.querySelector(".carousel__nav");
+const id = location.pathname.substring(location.pathname.lastIndexOf("/") + 1);
+const carousel__nav = document.querySelector(".carousel__nav");
 
-let url = `/api/attraction/`;
+const url = `/api/attraction/`;
 
-let attractionMessage = document.querySelector(".attraction-message");
-let description = document.querySelector(".description");
-let track = document.querySelector(".carousel__track");
-let oneHalfDay = document.querySelector(".oneHalfDay");
-let nextHalfDay = document.querySelector(".nextHalfDay");
-let totalPrice = document.querySelector(".totalPrice");
+const attractionMessage = document.querySelector(".attraction-message");
+const description = document.querySelector(".description");
+const track = document.querySelector(".carousel__track");
+const oneHalfDay = document.querySelector(".oneHalfDay");
+const nextHalfDay = document.querySelector(".nextHalfDay");
+const totalPrice = document.querySelector(".totalPrice");
 
-fetch(url + id)
-  .then((response) => response.json())
-  .then((data) => {
-    for (let i = 0; i < data.data.image.length; i++) {
-      track.insertAdjacentHTML(
-        "afterBegin",
-        `<li class="carousel__slide">
+const lay = document.querySelector(".lay");
+const signinPlace = document.querySelector("#signinPlace");
+const signupPlace = document.querySelector("#signupPlace");
+const signinBtn = document.querySelector("#signinBtn");
+const signupBtn = document.querySelector("#signupBtn");
+const signinText = document.querySelector(".signinText");
+const logoutText = document.querySelector(".logoutText");
+const signinMsg = document.querySelector(".signinMsg");
+const signupMsg = document.querySelector(".signupMsg");
+
+// sign up
+signupBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  let signupName = document.querySelector("#signupName").value;
+  let signupEmail = document.querySelector("#signupEmail").value;
+  let signupPassword = document.querySelector("#signupPassword").value;
+  fetch(`${location.origin}/api/user`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name: signupName,
+      email: signupEmail,
+      password: signupPassword,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.error) {
+        signupMsg.style.display = "block";
+        signupMsg.textContent = data.message;
+        window.setTimeout(hideMsg, 2000);
+      } else {
+        console.log(data);
+        signupMsg.style.display = "block";
+        signupMsg.textContent = data.message;
+        signupMsg.style.color = "green";
+      }
+    });
+});
+
+// signin
+signinBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  let signinEmail = document.querySelector("#signinEmail").value;
+  let signinPassword = document.querySelector("#signinPassword").value;
+  fetch(`${location.origin}/api/user/auth`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email: signinEmail, password: signinPassword }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.error) {
+        signinMsg.style.display = "block";
+        signinMsg.textContent = data.message;
+        window.setTimeout(hideMsg, 2000);
+      } else {
+        closeSignDialog();
+        console.log(data);
+        signinText.classList.add("hide");
+        logoutText.classList.remove("hide");
+        location.reload();
+      }
+    });
+});
+
+function hideMsg() {
+  signupMsg.style.display = "none";
+  signinMsg.style.display = "none";
+}
+
+window.addEventListener("load", (e) => {
+  getData();
+  getUser();
+});
+
+//點擊登入 跳出視窗
+function showSigninDialog() {
+  signupPlace.style.display = "none";
+  signinPlace.style.display = "block";
+  lay.classList.remove("hide");
+}
+
+// 點擊 點此註冊 隱藏登入框 顯示註冊框
+function showSignupDialog() {
+  signinPlace.style.display = "none";
+  signupPlace.style.display = "block";
+}
+//關閉註冊登入
+function closeSignDialog() {
+  signinPlace.style.display = "none";
+  signupPlace.style.display = "none";
+  lay.classList.add("hide");
+}
+
+//點擊登出系統
+logoutText.addEventListener("click", (e) => {
+  fetch(`${location.origin}/api/user/auth`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.ok) {
+        logoutText.classList.add("hide");
+        signinText.classList.remove("hide");
+        location.reload();
+      }
+    });
+});
+function getData() {
+  fetch(url + id)
+    .then((response) => response.json())
+    .then((data) => {
+      for (let i = 0; i < data.data.image.length; i++) {
+        track.insertAdjacentHTML(
+          "beforeEnd",
+          `<li class="carousel__slide">
             <img class="carousel__image" src=${data.data.image[i]} alt="" />
          </li>
         `
-      );
-      carousel__nav.insertAdjacentHTML(
-        "afterBegin",
-        `<button class="carousel__indicator"></button>`
-      );
-    }
-    // 第一個li & button 加上 current-slide
-    let firstSlide = document.getElementsByClassName("carousel__slide")[0];
-    let carousel__indicator = document.getElementsByClassName(
-      "carousel__indicator"
-    )[0];
-    firstSlide.classList.add("current-slide");
-    carousel__indicator.classList.add("current-slide");
+        );
+        carousel__nav.insertAdjacentHTML(
+          "afterBegin",
+          `<button class="carousel__indicator"></button>`
+        );
+      }
+      // 第一個li & button 加上 current-slide
+      let firstSlide = document.getElementsByClassName("carousel__slide")[0];
+      let carousel__indicator = document.getElementsByClassName(
+        "carousel__indicator"
+      )[0];
+      firstSlide.classList.add("current-slide");
+      carousel__indicator.classList.add("current-slide");
 
-    attractionMessage.insertAdjacentHTML(
-      "afterBegin",
-      ` <div class="attraction-data">
+      attractionMessage.insertAdjacentHTML(
+        "afterBegin",
+        ` <div class="attraction-data">
             <h3>${data.data.name}</h3>
             <p>${data.data.cat} at ${data.data.mrt}</p>
         </div>`
-    );
-    description.insertAdjacentHTML(
-      "afterBegin",
-      ` <p>
+      );
+      description.insertAdjacentHTML(
+        "afterBegin",
+        ` <p>
         ${data.data.description}
         </p>
         <h3>景點地址：</h3>
@@ -54,92 +171,93 @@ fetch(url + id)
         <h3>交通方式：</h3>
         <p>
         ${data.data.direction}
-        </p>`
-    );
+        </p>
+        <iframe width="900" height="600" frameborder="0" scrolling="no" marginheight="20px" marginwidth="10" src=https://maps.google.com.tw/maps?f=q&hl=zh-TW&geocode=&q=${data.data.latitude},${data.data.longitude}(${data.data.name})&z=16&output=embed&t=></iframe>
+        
+        `
+      );
 
-    let slides = Array.from(track.children);
-    // console.log(track);
-    let nextButton = document.querySelector(".carousel__button--right");
-    let prevButton = document.querySelector(".carousel__button--left");
-    let dotsNav = document.querySelector(".carousel__nav");
-    let dots = Array.from(dotsNav.children);
-    //取得圖片大小
-    let slideWidth = slides[0].getBoundingClientRect().width;
-    console.log(slideWidth);
+      let slides = Array.prototype.slice.call(track.children);
 
-    let setSlidePosition = (slide, index) => {
-      slide.style.left = slideWidth * index + "px";
-    };
-    slides.forEach(setSlidePosition);
+      let nextButton = document.querySelector(".carousel__button--right");
+      let prevButton = document.querySelector(".carousel__button--left");
+      let dotsNav = document.querySelector(".carousel__nav");
+      let dots = Array.from(dotsNav.children);
+      //取得圖片大小
+      let slideWidth = slides[0].getBoundingClientRect().width;
 
-    let moveToslide = (track, currentSlide, targetSlide) => {
-      track.style.transform = "translateX(-" + targetSlide.style.left + ")";
-      currentSlide.classList.remove("current-slide");
-      targetSlide.classList.add("current-slide");
-    };
-
-    let updateDots = (currentDot, targetDot) => {
-      currentDot.classList.remove("current-slide");
-      targetDot.classList.add("current-slide");
-    };
-
-    // 點擊左邊的按鈕往左滑
-    prevButton.addEventListener("click", (e) => {
-      //當前照片
-      let currentSlide = track.querySelector(".current-slide");
-      let prevSlide = currentSlide.previousElementSibling;
-      //當前小點點
-      let currentDot = dotsNav.querySelector(".current-slide");
-      let prevDot = currentDot.previousElementSibling;
-
-      let prevIndex = slides.findIndex((slide) => slide === prevSlide);
-      moveToslide(track, currentSlide, prevSlide);
-      updateDots(currentDot, prevDot);
-      hideShowArrows(slides, prevButton, nextButton, prevIndex);
-    });
-
-    // 點擊右邊按鈕往右滑
-    nextButton.addEventListener("click", (e) => {
-      let currentSlide = track.querySelector(".current-slide");
-      let nextSlide = currentSlide.nextElementSibling;
-      let currentDot = dotsNav.querySelector(".current-slide");
-      //下一個元素
-      let nextDot = currentDot.nextElementSibling;
-      let nextIndex = slides.findIndex((slide) => slide === nextSlide);
-      moveToslide(track, currentSlide, nextSlide);
-      updateDots(currentDot, nextDot);
-      hideShowArrows(slides, prevButton, nextButton, nextIndex);
-    });
-    // when i click the nav indicators ,move to the slide
-    dotsNav.addEventListener("click", (e) => {
-      //what indicator was click on?
-      let targetDot = e.target.closest("button");
-      //   console.log("test1");
-      //stop the function
-      if (!targetDot) return;
-      let currentSlide = track.querySelector(".current-slide");
-      let currentDot = dotsNav.querySelector(".current-slide");
-      let targetIndex = dots.findIndex((dot) => dot === targetDot);
-      let targetSlide = slides[targetIndex];
-
-      //   console.log(targetIndex);
-      moveToslide(track, currentSlide, targetSlide);
-      updateDots(currentDot, targetDot);
-      hideShowArrows(slides, prevButton, nextButton, targetIndex);
-    });
-    let hideShowArrows = (slides, prevButton, nextButton, targetIndex) => {
-      if (targetIndex === 0) {
-        prevButton.classList.add("is-hidden");
-        nextButton.classList.remove("is-hidden");
-      } else if (targetIndex === slides.length - 1) {
-        prevButton.classList.remove("is-hidden");
-        nextButton.classList.add("is-hidden");
-      } else {
-        prevButton.classList.remove("is-hidden");
-        nextButton.classList.remove("is-hidden");
+      //給定每個slide left
+      for (let i = 0; i < slides.length; i++) {
+        slides[i].style.left = slideWidth * i + "px";
       }
-    };
-  });
+
+      function moveToslide(track, currentSlide, targetSlide) {
+        track.style.transform = "translateX(-" + targetSlide.style.left + ")";
+        currentSlide.classList.remove("current-slide");
+        targetSlide.classList.add("current-slide");
+      }
+
+      function updateDots(currentDot, targetDot) {
+        currentDot.classList.remove("current-slide");
+        targetDot.classList.add("current-slide");
+      }
+
+      // 點擊左邊的按鈕往左滑
+      prevButton.addEventListener("click", (e) => {
+        //當前照片
+        let currentSlide = track.querySelector(".current-slide");
+        let prevSlide = currentSlide.previousElementSibling;
+        //當前小點點
+        let currentDot = dotsNav.querySelector(".current-slide");
+        let prevDot = currentDot.previousElementSibling;
+
+        let prevIndex = slides.findIndex((slide) => slide === prevSlide);
+        moveToslide(track, currentSlide, prevSlide);
+        updateDots(currentDot, prevDot);
+        hideShowArrows(slides, prevButton, nextButton, prevIndex);
+      });
+
+      // 點擊右邊按鈕往右滑
+      nextButton.addEventListener("click", (e) => {
+        let currentSlide = track.querySelector(".current-slide");
+        let nextSlide = currentSlide.nextElementSibling;
+        let currentDot = dotsNav.querySelector(".current-slide");
+        //下一個元素
+        let nextDot = currentDot.nextElementSibling;
+        let nextIndex = slides.findIndex((slide) => slide === nextSlide);
+        moveToslide(track, currentSlide, nextSlide);
+        updateDots(currentDot, nextDot);
+        hideShowArrows(slides, prevButton, nextButton, nextIndex);
+      });
+      // 點擊移動點點
+      dotsNav.addEventListener("click", (e) => {
+        //what indicator was click on?
+        let targetDot = e.target.closest("button");
+        //stop the function
+        if (!targetDot) return;
+        let currentSlide = track.querySelector(".current-slide");
+        let currentDot = dotsNav.querySelector(".current-slide");
+        let targetIndex = dots.findIndex((dot) => dot === targetDot);
+        let targetSlide = slides[targetIndex];
+
+        moveToslide(track, currentSlide, targetSlide);
+        updateDots(currentDot, targetDot);
+        hideShowArrows(slides, prevButton, nextButton, targetIndex);
+      });
+      let hideShowArrows = (slides, prevButton, nextButton, targetIndex) => {
+        if (targetIndex === 0) {
+          prevButton.classList.add("is-hidden");
+          nextButton.classList.remove("is-hidden");
+        } else if (targetIndex === slides.length - 1) {
+          prevButton.classList.remove("is-hidden");
+          nextButton.classList.add("is-hidden");
+        } else {
+          prevButton.classList.remove("is-hidden");
+          nextButton.classList.remove("is-hidden");
+        }
+      };
+    });
+}
 
 oneHalfDay.addEventListener("click", (e) => {
   totalPrice.textContent = "新台幣2000元";
@@ -148,3 +266,25 @@ oneHalfDay.addEventListener("click", (e) => {
 nextHalfDay.addEventListener("click", (e) => {
   totalPrice.textContent = "新台幣2500元";
 });
+
+// get user
+function getUser() {
+  fetch(`${location.origin}/api/user/auth`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      //未登入
+      if (!data.data) {
+        logoutText.classList.add("hide");
+        signinText.classList.remove("hide");
+      } else if (data.data) {
+        console.log(data);
+        signinText.classList.add("hide");
+        logoutText.classList.remove("hide");
+      }
+    });
+}
